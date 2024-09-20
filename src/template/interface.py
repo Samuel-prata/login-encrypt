15e463f7d4c0b2fae0a678ffa88257df5fe79a94
samuel-prata <samuel.silverio001@hotmail.com>
2024-09-20 20:10:26 -0300

    feat: tela de login

--- src/template/interface.py
+++ src/template/interface.py
@@ -1,21 +1,50 @@
 import streamlit as st
 import requests
 
-def get_response(user, pwd):
-    url = 'http://localhost:5000/user/login'
-    payload = {'usuario': user, 'senha': pwd}
+def get_response_register(name: str, last_name: str, email: str, pwd: str):
+    url = 'http://localhost:5000/user/register'
+    payload = {'name': name, 'last_name': last_name, 'email': email, 'password': pwd}
     response = requests.post(url, json=payload)
-    return response
+    return response.json()
 
-st.title('Tela de login')
+def get_response_login(email: str, pwd: str):
+    url = 'http://localhost:5000/user/login'
+    payload = {'email': email, 'password': pwd}
+    response = requests.post(url, json=payload)
+    return response.json()
 
-user = st.text_input('digite o nome de usuario')
-pwd = st.text_input('digite a senha: ')
+def screen_register(): 
+    st.title('Tela de registro')
 
-if st.button('Entrar'):
-    if user and pwd:
-        response = get_response(user, pwd)
-        st.write(response)
-    else:
-        st.write('Preencha os campos')
+    name = st.text_input('digite o seu nome: ')
+    last_name = st.text_input('Digite seu sobrenome: ')
+    email = st.text_input('Digite seu email: ')
+    pwd = st.text_input('digite a senha: ')
+
+    if st.button('Entrar'):
+        if name and last_name and email and pwd:
+            response = get_response_register(name, last_name, email, pwd)
+            st.write(response)
+        else:
+            st.write('Preencha os campos')
     
+def screen_login():
+    st.title('Tela de login')
+    
+    email = st.text_input('Digite seu email: ')
+    senha = st.text_input('Digite sua senha: ')
+    
+    if st.button('Entrar'):
+        if email and senha:
+            response = get_response_login(email, senha)
+            st.write(response)
+        else:
+            st.write('Preencha todos os campos')
+    
+st.sidebar.title('Navegação')
+page_selected = st.sidebar.selectbox('Escolha a opção', ['Sign in', 'Sign up'])
+
+if page_selected == 'Sign in':
+    screen_login()
+elif page_selected == 'Sign up':
+    screen_register()
